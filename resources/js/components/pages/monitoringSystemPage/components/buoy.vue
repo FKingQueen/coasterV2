0b5255167ca14a4821eb08d5fe1e85a86f7f0e8c
FKingQueen <luxborj@gmail.com>
2024-11-27 16:13:48 +0800

    11/27/2024

--- resources/js/components/pages/monitoringSystemPage/components/buoy.vue
+++ resources/js/components/pages/monitoringSystemPage/components/buoy.vue
@@ -47,6 +47,13 @@ export default defineComponent({
             ],
             type: '',
             loading: true,
+            series: [
+                    {
+                        name: "",
+                        data: [],  // your data will be filled dynamically
+                        type: '',
+                    }
+                ],
             chartOptions1: {
                 chart: {
                     backgroundColor: '#f7f7f7', // Light gray background
@@ -95,11 +102,11 @@ export default defineComponent({
                         data: [],  // your data will be filled dynamically
                         type: '',
                     },
-                    // {
-                    //     name: "",
-                    //     data: [],  // your data will be filled dynamically
-                    //     type: '',
-                    // },
+                    {
+                        name: "",
+                        data: [],  // your data will be filled dynamically
+                        type: '',
+                    },
                 ],
             },
         };
@@ -125,7 +132,7 @@ export default defineComponent({
                             thiss.setChartStyleWave();
                             thiss.chartOptions1.series[0].data = thiss.data.waveHeight
                             thiss.chartOptions1.series[1].data = thiss.data.wavePeriod
-                            // thiss.chartOptions1.series[2].data = thiss.data.compass
+                            thiss.chartOptions1.series[2].data = thiss.data.compass
 
                         });
 
@@ -240,16 +247,16 @@ export default defineComponent({
             thiss.chartOptions1.series[1].tooltip = { valueSuffix: 's', valueDecimals: 2 }
 
             // compass
-            // thiss.chartOptions1.series[2].type = 'windbarb'
-            // thiss.chartOptions1.series[2].name = 'Wind Direction'
-            // thiss.chartOptions1.series[2].id = 'windbarbs'
-            // thiss.chartOptions1.series[2].color = 'white'
-            // thiss.chartOptions1.series[2].lineWidth = 1.5
-            // thiss.chartOptions1.series[2].vectorLength = 15
-            // thiss.chartOptions1.series[2].yOffset = -15
-            // thiss.chartOptions1.series[2].tooltip = {
-            //     valueSuffix: 'Knots'
-            // }
+            thiss.chartOptions1.series[2].type = 'windbarb'
+            thiss.chartOptions1.series[2].name = 'Wind Direction'
+            thiss.chartOptions1.series[2].id = 'windbarbs'
+            thiss.chartOptions1.series[2].color = 'white'
+            thiss.chartOptions1.series[2].lineWidth = 1.5
+            thiss.chartOptions1.series[2].vectorLength = 15
+            thiss.chartOptions1.series[2].yOffset = -15
+            thiss.chartOptions1.series[2].tooltip = {
+                valueSuffix: 'Knots'
+            }
 
             //     name: 'Wind',
             //     type: 'windbarb',
