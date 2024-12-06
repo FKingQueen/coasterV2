126fa25573bec1c37862ae69a8c8e44e7cff90a6
Patrick Borja <luxborj@gmail.com>
2024-12-06 15:45:14 +0800

    06/12/2024

--- resources/js/components/pages/homePage/hazardMaps/hazardMaps.vue
+++ resources/js/components/pages/homePage/hazardMaps/hazardMaps.vue
@@ -1,9 +1,13 @@
 <template>
+
     <div class="w-full flex">
-        <a-checkbox-group v-model:value="checkedValues" @change="onCheck()" :options="plainOptions"
-            class="vertical-checkbox-group space-y-2" />
-        <div ref="map" class="full-screen-map w-full">
-        </div>
+        <a-checkbox-group
+            v-model:value="checkedValues"
+            @change="onCheck()"
+            :options="plainOptions"
+            class="vertical-checkbox-group space-y-2"
+        />
+        <div ref="map" class="full-screen-map w-full"></div>
         <div class="overlay-container p-1" id="popup">
             <span id="feature-additional-info"></span>
             <div class="flex justify-between w-full items-center">
@@ -11,17 +15,20 @@
                 <button @click="closePopup()" class="capitalize">x</button>
             </div>
             <div class="flex justify-center">
-                <Table height="200" :columns="columns" :data="data" class="w-full"></Table>
+                <Table
+                    height="200"
+                    :columns="columns"
+                    :data="data"
+                    class="w-full"
+                ></Table>
             </div>
-
         </div>
     </div>
 </template>
 
 <script>
-
 // Vue Libraries
-import { defineComponent, ref, nextTick, h } from 'vue';
+import { defineComponent, ref, nextTick, h } from "vue";
 // OpenLayers Libraries
 
 import "ol/ol.css";
@@ -29,16 +36,16 @@ import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
 import FullScreen from "ol/control/FullScreen";
-import { fromLonLat } from 'ol/proj';
+import { fromLonLat } from "ol/proj";
 import { Fill, Style, Stroke } from "ol/style";
 // import VectorLayer from "ol/layer/Vector"; // Correct import statement
 // import VectorSource from "ol/source/Vector"; // Add VectorSource import
-import XYZ from 'ol/source/XYZ';
+import XYZ from "ol/source/XYZ";
 // import GeoJSON from 'ol/format/GeoJSON';
 
 // Datatables
-import { DataTable } from 'datatables.net-vue3';
-import Select from 'datatables.net-select';
+import { DataTable } from "datatables.net-vue3";
+import Select from "datatables.net-select";
 // Components
 
 // Overview Component
@@ -48,12 +55,10 @@ import VectorSource from "ol/source/Vector";
 import GeoJSON from "ol/format/GeoJSON";
 import LayerGroup from "ol/layer/Group";
 
-import VectorTileLayer from 'ol/layer/VectorTile'
-import VectorTileSource from 'ol/source/VectorTile'
-import MVT from 'ol/format/MVT'
-import TileWMS from 'ol/source/TileWMS';
-
-
+import VectorTileLayer from "ol/layer/VectorTile";
+import VectorTileSource from "ol/source/VectorTile";
+import MVT from "ol/format/MVT";
+import TileWMS from "ol/source/TileWMS";
 
 export default defineComponent({
     components: {
@@ -62,29 +67,28 @@ export default defineComponent({
     setup() {
         const visible = ref(false);
         const plainOptions = [
-            'End point Rate (m/yr) of Region 1, Philippines',
-            'Net shorline movement (m) of Region 1, Philippines',
-        ]
+            "End point Rate (m/yr) of Region 1, Philippines",
+            "Net shorline movement (m) of Region 1, Philippines",
+        ];
 
-        const checkedValues = ref([])
+        const checkedValues = ref([]);
         return {
             visible,
             popup: null,
             plainOptions,
-            checkedValues
+            checkedValues,
         };
     },
     data() {
-
         return {
             columns: [
                 {
-                    title: 'Attribute',
-                    key: 'attribute'
+                    title: "Attribute",
+                    key: "attribute",
                 },
                 {
-                    title: 'Value',
-                    key: 'value'
+                    title: "Value",
+                    key: "value",
                 },
             ],
             data: [],
@@ -92,8 +96,7 @@ export default defineComponent({
             nsmLayer: null,
             eprLayer: null,
             groupLayer: null,
-
-        }
+        };
     },
     async mounted() {
         this.initializeMap();
@@ -103,34 +106,34 @@ export default defineComponent({
             // console.log(this.checkedValues);
             // console.log(this.nsmLayer.get('title') );
             // console.log(this.groupLayer);
-            const layers = this.groupLayer.getLayers()
+            const layers = this.groupLayer.getLayers();
 
             // Convert layers to an array if it's not already
-            const layersArray = layers.getArray()
+            const layersArray = layers.getArray();
 
             // Iterate through layers
-            layersArray.forEach(layer => {
+            layersArray.forEach((layer) => {
                 // Get the layer's title
-                const layerTitle = layer.get('title')
+                const layerTitle = layer.get("title");
                 // Check if the layer's title is in the checked values
-                if (layerTitle != 'basemap1') {
-                    const isVisible = this.checkedValues.includes(layerTitle)
+                if (layerTitle != "basemap1") {
+                    const isVisible = this.checkedValues.includes(layerTitle);
                     // Set layer visibility
-                    layer.setVisible(isVisible)
+                    layer.setVisible(isVisible);
                 }
-            })
-
+            });
         },
         initializeMap() {
             // Define the tile layers
             // mapbox://styles/pcborja/clt6v3fh3003d01r58rcg2h8p
             // mapbox://styles/pcborja/clt6utkbc00g801raamfl71ab
-            const satelliteLayer = new TileLayer({
+            // mapbox://styles/pcborja/clt6utkbc00g801raamfl71ab
+            const basemap1 = new TileLayer({
                 source: new XYZ({
                     url: "https://api.mapbox.com/styles/v1/pcborja/clt6utkbc00g801raamfl71ab/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGNib3JqYSIsImEiOiJjbG5sZm9weGIxYzg4MmxtbmpqYjd2YXIxIn0.LmH0x1Rn3NDzJdzq3J6Ayg",
                 }),
                 visible: true,
-                title: 'basemap1',
+                title: "basemap1",
             });
 
             // Define GeoJSON layers
@@ -163,7 +166,6 @@ export default defineComponent({
             //     visible: true,
             //     title: 'basemap1'
             // });
-
 
             // new TileLayer({
             //     source: new XYZ({
@@ -213,22 +215,30 @@ export default defineComponent({
             //     },
             // })
 
-
             const mapboxLayer = new VectorTileLayer({
                 source: new VectorTileSource({
                     // url: 'http://localhost:3650/api/tiles/IlocosNorte_EPR/{z}/{x}/{y}',
                     // url: `shapefile/PBF/IlocosNorteEPR/{z}/{x}/{y}.pbf`,
-                    url: `http://localhost:8080/data/IN_EPR/{z}/{x}/{y}.pbf`,
+                    url: `http://localhost:3650/api/tiles/100yrs/{z}/{x}/{y}`,
                     format: new MVT(),
                 }),
                 visible: true,
                 style: (feature) => {
-                    let propertyValue = feature.get('EPR_trend');
 
-                    const fillColor = propertyValue === 'stable' ? '#407f3e'
-                        : (propertyValue === 'erosion' ? '#f94449' : '#1260cc');
-                    const strokeColor = propertyValue === 'stable' ? '#407f3e'
-                        : (propertyValue === 'erosion' ? '#f94449' : '#1260cc');
+                    let propertyValue = feature.get("6D0DB100 ");
+
+                    const fillColor =
+                        propertyValue === "Low"
+                            ? "#FFFF00"
+                            : propertyValue === "Medium"
+                            ? "#FFA500"
+                            : "#FF0000";
+                    const strokeColor =
+                        propertyValue === "Low"
+                            ? "#FFFF00"
+                            : propertyValue === "Medium"
+                            ? "#FFA500"
+                            : "#FF0000";
                     return new Style({
                         fill: new Fill({
                             color: fillColor,
@@ -239,8 +249,7 @@ export default defineComponent({
                         }),
                     });
                 },
-            })
-
+            });
 
             // EPR Region 1
             this.eprLayer = new VectorLayer({
@@ -258,12 +267,12 @@ export default defineComponent({
                     }),
                 }),
                 visible: false,
-                title: 'End point Rate (m/yr) of Region 1, Philippines'
+                title: "End point Rate (m/yr) of Region 1, Philippines",
             });
 
             // Create a group layer
             this.groupLayer = new LayerGroup({
-                layers: [satelliteLayer, this.eprLayer, mapboxLayer],
+                layers: [basemap1, mapboxLayer],
             });
 
             // Initialize the map
@@ -276,7 +285,7 @@ export default defineComponent({
                 }),
                 controls: [new FullScreen()],
             });
-            const overlayContainerElement = document.getElementById('popup');
+            const overlayContainerElement = document.getElementById("popup");
 
             // Create a popup overlay
             this.overlayLayer = new Overlay({
@@ -285,58 +294,58 @@ export default defineComponent({
                 // stopEvent: false,
             });
 
-            this.map.addOverlay(this.overlayLayer)
+            this.map.addOverlay(this.overlayLayer);
 
-            const overlayTitle = document.getElementById('layerTitle');
+            const overlayTitle = document.getElementById("layerTitle");
 
             // Add a click event to show the popup
             this.map.on("click", (event) => {
                 let featureFound = false;
 
-                this.map.forEachFeatureAtPixel(event.pixel, (feature, layer) => {
-                    console.log(feature.getProperties());
+                this.map.forEachFeatureAtPixel(
+                    event.pixel,
+                    (feature, layer) => {
+                        console.log(feature.getProperties());
 
+                        const properties = feature.getProperties();
+                        console.log("properties: ", properties);
+                        const layerTitle = layer.get("title");
 
-                    const properties = feature.getProperties();
-                    console.log('properties: ', properties);
-                    const layerTitle = layer.get('title');
+                        const formattedData = Object.entries(properties)
+                            .filter(([key]) => key !== "geometry") // Exclude geometry
+                            .map(([key, value]) => ({
+                                attribute: key,
+                                value: value,
+                            }));
 
-                    const formattedData = Object.entries(properties)
-                        .filter(([key]) => key !== 'geometry') // Exclude geometry
-                        .map(([key, value]) => ({
-                            attribute: key,
-                            value: value
-                        }));
+                        // Populate the data array
+                        this.data = formattedData;
 
-                    // Populate the data array
-                    this.data = formattedData;
+                        overlayTitle.innerHTML = `Layer Name: ` + layerTitle;
+                        let coordinate = event.coordinate;
+                        // let coordinate = event.coordinate;
+                        // console.log(properties);
+                        this.overlayLayer.setPosition(coordinate);
+                        featureFound = true;
 
-                    overlayTitle.innerHTML = `Layer Name: ` + layerTitle
-                    let coordinate = event.coordinate;
-                    // let coordinate = event.coordinate;
-                    // console.log(properties);
-                    this.overlayLayer.setPosition(coordinate);
-                    featureFound = true;
-
-                    this.map.getView().animate({
-                        center: coordinate,
-                        duration: 500 // Optional: smooth animation duration
-                    });
-                });
-
-
+                        this.map.getView().animate({
+                            center: coordinate,
+                            duration: 500, // Optional: smooth animation duration
+                        });
+                    }
+                );
 
                 // Hide the popup if no feature was found
                 if (!featureFound) {
-                    this.data = []
+                    this.data = [];
                     this.overlayLayer.setPosition(undefined);
                 }
             });
         },
         closePopup() {
             this.overlayLayer.setPosition(undefined);
-        }
-    }
+        },
+    },
 });
 </script>
 
@@ -363,7 +372,7 @@ export default defineComponent({
 }
 
 .overlay-container #feature-additional-info::after {
-    content: '';
+    content: "";
     position: absolute;
     top: 100%;
     left: 50%;
